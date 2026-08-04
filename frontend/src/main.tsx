01ef8d4086f54343fdc031345c6a976cb0105eb6
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-04 15:11:04 +0500

    feat: add routing with AppRoutes component and integrate react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {BrowserRouter as Router} from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+   <Router>
+        <AppRoutes />
+   </Router>
 )
