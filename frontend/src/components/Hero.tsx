a5515cd8aba0ee3e964c14023b9c9900aee51ebb
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-04 16:53:16 +0500

    feat: add HomePage and Hero components, update AppRoutes to include HomePage

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+// @ts-ignore
+import hero from "../assets/hero.png"
+const Hero = () => {
+    return (
+        <div>
+            <img src={hero} className="w-full max-h-150 object-cover"/>
+
+        </div>
+    )
+}
+export default Hero
