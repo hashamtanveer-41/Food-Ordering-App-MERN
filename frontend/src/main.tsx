4940c9c4cfc40b9f0f58ff9bf200bdbee3ecf6cf
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-04 18:56:47 +0500

    feat: add Auth0ProviderWithNavigate component and integrate it into main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter as Router} from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "@/auth/Auth0ProviderWithNavigate.tsx";
 
 createRoot(document.getElementById('root')!).render(
    <Router>
-        <AppRoutes />
+       <Auth0ProviderWithNavigate>
+           <AppRoutes />
+       </Auth0ProviderWithNavigate>
    </Router>
 )
