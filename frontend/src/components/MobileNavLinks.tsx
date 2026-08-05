006c29da94f11e94a3396faab3bae247276cad6a
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-05 22:44:33 +0500

    feat: enhance mobile navigation with user authentication and links

---
+++ frontend/src/components/MobileNavLinks.tsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+import {Button} from "@/components/ui/button.tsx";
+import {useAuth0} from "@auth0/auth0-react";
+
+const MobileNavLinks = () => {
+    const {logout}= useAuth0();
+
+    return (
+        <>
+            <Link to='/user-profile' className="flex bg-white font-bold items-center hover:text-orange-500 ">
+                User Profile
+            </Link>
+            <Button onClick={()=>logout()} className="flex items-center px-3 font-bold hover:bg-gray500">
+                Log Out
+            </Button>
+        </>
+    )
+}
+export default MobileNavLinks
