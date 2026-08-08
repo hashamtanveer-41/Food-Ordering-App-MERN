e3e84ad415d85a0dd76f58ed1503e8fbdb6917c8
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-08 22:53:22 +0500

    feat: add user profile form with validation using react-hook-form and zod

---
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -0,0 +1,96 @@
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel} from "@/components/ui/form";
+import {Input} from "@/components/ui/input.tsx";
+
+const formSchema = z.object({
+    email: z.string().optional(),
+    name: z.string().min(1, "Name is required").max(100, "Name must be less than 100 characters"),
+    addressLine1: z.string().min(1, "Address is required"),
+    city: z.string().min(1, "City is required"),
+    country: z.string().min(1, "Country is required"),
+})
+
+type UserFormData = z.infer<typeof formSchema>;
+
+type Props = {
+    onSave: (userProfileData: UserFormData)=>void;
+    isLoading: boolean;
+}
+const UserProfileForm = ({onSave, isLoading}: Props) => {
+    const form = useForm<UserFormData>({
+        resolver: zodResolver(formSchema),
+    });
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSave)} className="space-y-4 bg-gray-50 rounded-lg md:p-10">
+                <div>
+                    <h2 className="text-2xl font-bold">User Profile Form</h2>
+                    <FormDescription>
+                        View and change your profile information here
+                    </FormDescription>
+                </div>
+                <FormField control={form.control} render={({field})=>(
+                    <FormItem>
+                        <FormLabel>
+                            Email
+                        </FormLabel>
+                        <FormControl>
+                            <Input {...field} className="bg-white"/>
+                        </FormControl>
+                    </FormItem>
+                )} name="email"
+                />
+                <FormField control={form.control} render={({field})=>(
+                    <FormItem>
+                        <FormLabel>
+                            Name
+                        </FormLabel>
+                        <FormControl>
+                            <Input {...field} className="bg-white"/>
+                        </FormControl>
+                    </FormItem>
+                )} name="name"
+                />
+
+                <div className="flex flex-col md:flex-row gap-4">
+                    <FormField control={form.control} render={({field})=>(
+                        <FormItem>
+                            <FormLabel>
+                                Address Line 1
+                            </FormLabel>
+                            <FormControl>
+                                <Input {...field} className="bg-white"/>
+                            </FormControl>
+                        </FormItem>
+                    )} name="addressLine1"
+                    />
+                    <FormField control={form.control} render={({field})=>(
+                        <FormItem>
+                            <FormLabel>
+                                City
+                            </FormLabel>
+                            <FormControl>
+                                <Input {...field} className="bg-white"/>
+                            </FormControl>
+                        </FormItem>
+                    )} name="city"
+                    />
+                    <FormField control={form.control} render={({field})=>(
+                        <FormItem>
+                            <FormLabel>
+                                Country
+                            </FormLabel>
+                            <FormControl>
+                                <Input {...field} className="bg-white"/>
+                            </FormControl>
+                        </FormItem>
+                    )} name="country"
+                    />
+                </div>
+            </form>
+        </Form>
+    )
+}
+export default UserProfileForm;
