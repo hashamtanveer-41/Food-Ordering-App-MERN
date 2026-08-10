fa11463952ca8c2f44941b8bf63aaa8bef03d14f
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-10 19:14:08 +0500

    feat: add ProtectedRoutes component to secure user profile access

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,13 +3,16 @@ import Layout from "@/layouts/layout";
 import HomePage from "@/pages/HomePage";
 import AuthCallBackPage from "@/pages/AuthCallBackPage.tsx";
 import UserProfilePage from "@/pages/UserProfilePage.tsx";
+import ProtectedRoutes from "@/auth/ProtectedRoutes.tsx";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path='/auth-callback' element={<AuthCallBackPage/>}/>
             <Route path='/' element={<Layout showHero={true}><HomePage /></Layout>}/>
-            <Route path='/user-profile' element={<Layout><UserProfilePage /></Layout>}/>
+            <Route element={<ProtectedRoutes />}>
+                <Route path='/user-profile' element={<Layout><UserProfilePage /></Layout>}/>
+            </Route>
             <Route path='*' element={<Navigate to='/' />} />
         </Routes>
     )
