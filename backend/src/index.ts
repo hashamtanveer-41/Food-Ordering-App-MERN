4db568fe9b449ef83e881bb92bf65a5aab0b31bd
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-19 00:11:52 +0500

    feat: add static file serving and error handling middleware in server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import "dotenv/config"
 import express, {type NextFunction,type Request,type Response} from "express";
 import cors from "cors";
+import path from "path";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.ts";
 import orderRoutes from "./routes/orderRoute.ts";
 import {v2 as cloudinary} from "cloudinary";
 import restaurantRoutes from "./routes/resturantRoutes.ts";
+import {fileURLToPath} from "url";
 
 mongoose.connect(process.env.MONGODB_URI as string).then(()=>{
     console.log("Connected to MongoDB");
@@ -23,6 +25,10 @@ const app = express();
 app.use(cors());
 const PORT = 8000;
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/api/order/checkout/webhook", express.raw({type: "*/*"}));
 
 app.use(express.json());
@@ -33,6 +39,21 @@ app.get("/health", async (req: Request, res:Response, next:NextFunction)=>{
 app.use("/api/users", userRoutes);
 app.use("/api/restaurants", restaurantRoutes);
 app.use("/api/order", orderRoutes);
+
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+app.get(/(.*)/,(req:Request,res:Response)=>{
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+})
+
+app.use((err: any, req: Request, res:Response, next:NextFunction)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+} )
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
